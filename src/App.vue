69432f3ff4a15bf274162a785c3466746161173e
Matias Bueno <matiascristobalbueno@gmail.com>
2024-06-21 03:11:32 -0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import carnet from "./components/carnet.vue";
+export default {
+    name: "App",
+    components: {
+        carnet
+    }
+}
+</script>
+
+<template>
+    <carnet />
+</template>
+
+<style scoped></style>
